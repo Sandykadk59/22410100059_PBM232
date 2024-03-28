d842790ab9a394bf74e01504b509308ad8bbb8a9
SandykaDK <22410100059@dinamika.ac.id>
2024-03-28 10:29:48 +0700

    Pertemuan 2

--- bin/latihan05.dart
+++ bin/latihan05.dart
@@ -1,17 +1,17 @@
 void main(){
   var x = [2,3,4];
 
-  var nama = <String>{"Adi","Budi","Cindi"};
-  var nama2 = new Set();
-
-  var job = {'nama' : 'lala', 'Kerja' : 'Programmer'};
-  var job2 = new Map();
-
   x.add(5);
   print(x[2]);
   print(x.length);
   x.removeAt(1);
   print(x.length);
+
+  var nama = <String>{"Adi","Budi","Cindi"};
+  var nama2 = new Set();
+
+  var job = {'nama' : 'Sandyka', 'Kerja' : 'Programmer'};
+  var job2 = new Map();
 
   print(nama);
   print(nama.elementAt(1));
