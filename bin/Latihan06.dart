8c379cb7f92b86296e072754506815ed6cb4223d
SandykaDK <22410100059@dinamika.ac.id>
2024-04-03 20:53:32 +0700

    Pertemuan 2

--- bin/Latihan06.dart
+++ bin/Latihan06.dart
@@ -13,4 +13,19 @@ void main(){
   //Final didapatkan setelah running
   final tgl = DateTime.now();
   print(tgl);
+
+  String kata = 'Dart adalah dasar flutter';
+  for(int i=0 ; i<kata.length ; i++){
+    if(kata[i]=='s'){
+      print('Nomor index s pada kata adalah $i');
+      break;
+    }
+  }
+
+  for(int i=0 ; i<10 ; i++){
+    if(i%2==0){
+      continue;
+    }
+    print(i);
+  }
 }
